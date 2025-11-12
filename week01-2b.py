c60b7eab76d09650b60efd159f134cdf56304c86
Wjxgin <a0925513822@gmail.com>
2025-11-12 11:54:18 +0800

    week01

--- week01-2b.py
+++
@@ -1,5 +0,0 @@
-# week01-2b.py LeetCode 學習計畫 python 版本1 for迴圈
-# find-the-index-of-the-first-occurrence-in-a-string
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle) #只有一行
